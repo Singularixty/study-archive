dedf91710e8ce7db65f17b42f84095a27a7851e8
Singularixty <135809938+Singularixty@users.noreply.github.com>
2024-08-26 20:55:22 +0700

    python categorized

---
+++ Languages/Python/Control Flow/for_loop.py
@@ -0,0 +1,9 @@
+items = {
+    "Item1" : "Sword",
+    "Item2" : "Potion",
+    "Item3" : "DP-50",
+    "Item4" : "AK-47"
+}
+
+for i, item_name in items.items():
+    print(i, "-", item_name)
---
+++ Languages/Python/Control Flow/pass.py
@@ -0,0 +1,9 @@
+list_of_odd = []
+
+for i in range(40, 100):
+    if i % 2 == 0:
+        pass
+    else:
+        list_of_odd.append(i)
+
+print(list_of_odd)
